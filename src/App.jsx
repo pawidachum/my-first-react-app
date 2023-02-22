11091b33eda28628d2115e7dee78fbfdc78ba8d0
pawida chumpurat <pawida.may@gmail.com>
2023-02-22 13:33:22 +0700

    feat: add my first react app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <section>
+	    <h1>My First React App on Vite</h1>
+      </section>
+    </div>
+  )
+}
+
+export default App
